f526f389c8529b121e24ff1c379f2b13e34d341f
Ritik <ritik.kanswal.5@gmail.com>
2021-08-07 10:55:14 +0530

    Intial Commit

---
+++ Random Leetcode Questions/12.Integer_to_Roman.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string res;
+        string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        for(int i=0;i<13;i++)
+        {
+            while(num>=values[i])
+            {
+                num-=values[i];
+                res+=symbol[i];
+            }
+        }
+        
+        return res;
+    }
+};
