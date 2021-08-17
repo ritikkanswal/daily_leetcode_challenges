3557154e396704871a2c7276fdd5c91aeb04bbc3
Ritik <ritik.kanswal.5@gmail.com>
2021-08-17 19:21:24 +0530

    Intial Commit

---
+++ Random Leetcode Questions/153.Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int binarysearch(vector<int>& nums,int left,int right)
+    {
+        if(right<left) return 0;
+        int mid=left+(right-left)/2;
+        int N=nums.size();
+        int prev=nums[(mid+N-1)%N];
+        int next=nums[(mid+1)%N];
+        if(nums[mid]<prev and nums[mid]<next)
+            return mid;
+        if(nums[0]>nums[mid])
+            return binarysearch(nums,left,mid-1);
+        else
+            return binarysearch(nums,mid+1,right);
+    }
+    int findMin(vector<int>& nums) {
+        int pivot=binarysearch(nums,0,nums.size()-1);
+        return nums[pivot];
+    }
+};
