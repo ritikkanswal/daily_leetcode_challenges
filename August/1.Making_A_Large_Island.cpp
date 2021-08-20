0619ac7885cd473fd574e43603729cb1dbbd662e
Ritik <ritik.kanswal.5@gmail.com>
2021-08-20 20:08:39 +0530

    Second COmmit

--- August/1.Making_A_Large_Island.cpp
+++ August/1.Making_A_Large_Island.cpp
@@ -1,32 +1,72 @@
-class Solution
-{
+class Solution {
 public:
-    int get(int i, int j, vector<vector<int>> &g)
+    bool visited[502][502];
+    int k=2;
+    map<int,int>mp;
+    int dfs(vector<vector<int>>&grid,int i,int j,int k)
     {
-        return (i < 0 || j < 0 || i >= g.size() || j >= g[0].size()) ? 0 : g[i][j];
-    }
-    int paint(int i, int j, int clr, vector<vector<int>> &g)
-    {
-        if (get(i, j, g) != 1)
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||!grid[i][j]||visited[i][j])
             return 0;
-        g[i][j] = clr;
-        return 1 + paint(i + 1, j, clr, g) + paint(i - 1, j, clr, g) + paint(i, j + 1, clr, g) + paint(i, j - 1, clr, g);
+        visited[i][j]=1;
+        grid[i][j]=k;
+        return 1+dfs(grid,i+1,j,k)+dfs(grid,i-1,j,k)+dfs(grid,i,j+1,k)+dfs(grid,i,j-1,k);
     }
-    int largestIsland(vector<vector<int>> &g, int res = 0)
-    {
-        vector<int> sizes = {0, 0}; // sentinel values; colors start from 2.
-        for (auto i = 0; i < g.size(); ++i)
-            for (auto j = 0; j < g[i].size(); ++j)
-                if (g[i][j] == 1)
-                    sizes.push_back(paint(i, j, sizes.size(), g));
-        for (auto i = 0; i < g.size(); ++i)
-            for (auto j = 0; j < g[i].size(); ++j)
-                if (g[i][j] == 0)
+    int largestIsland(vector<vector<int>>& grid) {
+        int r=grid.size();
+        int c=grid[0].size();
+        int ma=0;
+        bool flag=0;
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]==1 and !visited[i][j])
                 {
-                    unordered_set<int> s = {get(i + 1, j, g), get(i - 1, j, g), get(i, j + 1, g), get(i, j - 1, g)};
-                    res = max(res, 1 + accumulate(begin(s), end(s), 0, [&](int a, int b)
-                                                  { return a + sizes[b]; }));
+                    int area=dfs(grid,i,j,k);
+                    mp[k++]=area;
                 }
-        return res == 0 ? g.size() * g[0].size() : res;
+            }
+        }
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(grid[i][j]==0)
+                {
+                    map<int,int>mp2;
+                    int area=1;
+                    if((i-1)>=0)
+                    {
+                        if(!mp2[grid[i-1][j]])
+                        area+=mp[grid[i-1][j]];
+                        mp2[grid[i-1][j]]++;
+                    }
+                        
+                    if((j-1)>=0)
+                    {
+                        if(!mp2[grid[i][j-1]])
+                        area+=mp[grid[i][j-1]];
+                        mp2[grid[i][j-1]]++;
+                    }
+                    if((i+1)<r)
+                    {
+                        if(!mp2[grid[i+1][j]])
+                        area+=mp[grid[i+1][j]];
+                        mp2[grid[i+1][j]]++;
+                    }
+                    if((j+1)<c)
+                    {
+                        if(!mp2[grid[i][j+1]])
+                        area+=mp[grid[i][j+1]];
+                        mp2[grid[i][j+1]]++;
+                    }
+                    ma=max(ma,area);
+                    flag=1;
+                }
+            }
+        }
+        if(!flag)
+            return r*c;
+        return ma;
     }
 };
