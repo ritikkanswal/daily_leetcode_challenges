369f0c66b075be0deff6e2e067bf961c1ef89569
Ritik <ritik.kanswal.5@gmail.com>
2021-10-06 10:30:57 +0530

    Intial Commit

---
+++ October/6.Find All Duplicates in an Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int>res;
+        for(int i=0;i<nums.size();i++)
+        {
+            int x=abs(nums[i]);
+            if(nums[x-1]<0)
+                res.push_back(x);
+            else
+                nums[x-1]*=-1;
+        }
+        return res;
+    }
+};
